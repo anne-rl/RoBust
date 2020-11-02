bba4aaf84b72860ad1205d0804fa225e0a7ce5f3
Kenneth Verdida <kennethverds@github.com>
2020-11-02 09:09:04 +0800

    Update RoBust

---
+++ MyRoBust/user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+app_name = 'user'
+
+urlpatterns = [
+	path('landing', views.LandingIndexView.as_view(), name="landing_view"),
+]
