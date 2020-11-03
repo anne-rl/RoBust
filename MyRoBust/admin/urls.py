f386d3089afaf1d4e46d52d5d7fbbc6a7c189c3e
Kenneth Verdida <kennethverds@github.com>
2020-11-03 19:43:20 +0800

    All User Pages Update

---
+++ MyRoBust/admin/urls.py
@@ -0,0 +1,12 @@
+#from django.urls import path
+#from . import views
+#from django.conf.urls.static import static
+#from django.conf import settings
+#
+#app_name = 'admin'
+#
+#urlpatterns = [
+#    path('adminReservation', views.AdminReservationView.as_view(), name="adminReservation_view"),
+#    path('adminHistory', views.AdminHistoryView.as_view(), name="adminHistory_view"),
+#    path('adminDashboard', views.AdminDashboardView.as_view(), name="adminDashboard_view")
+#]
