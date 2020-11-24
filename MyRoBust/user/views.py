ddab7103a595204a1a86a30a2fcbef24da272c96
erwinvillarojo <erwinvillarojo@users.noreply.github.com>
2020-11-24 09:00:02 +0800

    FIle Update

--- MyRoBust/user/views.py
+++ MyRoBust/user/views.py
@@ -1,12 +1,14 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from django.views.generic import View
+
 from django.http import HttpResponse
 from django.contrib import messages
 
 
 class LandingIndexView(View):
   def get(self, request):
+
     return render(request, 'user/landing.html')
   
   def post(self, request):
