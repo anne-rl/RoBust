8eb6398191e3489d4ae19c34a23130732626cb21
Mary Anne Labasan <anne-rl@users.noreply.github.com>
2020-11-25 15:19:20 +0800

    update bus

--- MyRoBust/head/views.py
+++ MyRoBust/head/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.http import HttpResponse
 from django.contrib import messages
-from .forms import *
 from .models import *
+from .forms import BusForm
 
 
 
@@ -53,35 +53,6 @@ class HeadTripView(View):
     def post(self, request):
         return render(request, 'head/headTrip.html')
 
-class HeadDriverBusRegistration(View):
-    def get(self, request):
-        return render(request, 'head/headDriverBusRegistration.html')
-  
-    def post(self, request):
-        form = RegisterDriverForm(request.POST)
-    
-        if form.is_valid():
-            DriverFirstName = request.POST.get("firstName")
-            DriverMiddleName = request.POST.get("middleName")
-            DriverLastName = request.POST.get("lastName")
-            DriverEmailAddress = request.POST.get("emailAddress")
-            DriverContactNumber = request.POST.get("contactNumber")
-            DriverGender = request.POST.get("gender")
-            BusName = request.POST.get("busName")
-            BusDestination = request.POST.get("destination")
-            BusPlateNumber = request.POST.get("plateNumber")
-            BusSeats = request.POST.get("seats")
-            BusFare = request.POST.get("busFare")
-            BusTimeDeparture = request.POST.get("timeDeparture")
-
-            form = Driver(firstName = DriverFirstName, middleName = DriverMiddleName, lastName = DriverLastName, emailAddress = DriverEmailAddress, contactNumber = DriverContactNumber, gender = DriverGender, busName = BusName, destination = BusDestination, plateNumber = BusPlateNumber, seats = BusSeats, busFare = BusFare, timeDeparture = BusTimeDeparture)
-            form.save()
-        
-            return redirect('head:headList_view')   
-        else:
-            print(form.errors)
-            return HttpResponse('not valid')
-
 class HeadDashboardWeekly(View):
     def get(self, request):
         return render(request, 'head/headDashboardWeekly.html')
@@ -98,7 +69,63 @@ class HeadDashboardMonthly(View):
 
 class HeadUpdateBusView(View):
     def get(self, request):
-        return render(request, 'head/headUpdateBus.html')
+        qs_bus = Bus.objects.all()
+        context = {
+
+            'buses' : qs_bus
+
+        }
+        return render(request, 'head/headUpdateBus.html', context)
   
     def post(self, request):
-        return render(request, 'head/headUpdateBus.html')
+        return render(request, 'head/headUpdateBus.html', context)
+       
+class HeadRegisterBus(View):
+    def get(self, request):
+        return render(request, 'head/headRegisterBus.html')
+
+    def post(self, request):
+
+        form = BusForm(request.POST)
+
+        if form.is_valid():
+
+            bn = request.POST.get("busName")
+            pn = request.POST.get("plateNumber")
+            d = request.POST.get("destination")
+            ts = request.POST.get("totalSeats")
+            f = request.POST.get("busFare")
+            dt = request.POST.get("departureTime")
+            img = request.FILES.get('img')
+            form = Bus(busName = bn, plateNumber = pn, destination = d,
+                        totalSeats = ts, busFare = f, departureTime = dt, img = img)
+            form.save()
+
+            return redirect('head:headUpdateBus_view')
+
+        else:
+            print(form.errors)
+            return HttpResponse('not valid')
+class HeadRegisterDriver(View):
+    def get(self, request):
+        return render(request, 'head/headDriverBusRegistration.html')
+  
+    def post(self, request):
+        form = RegisterDriverForm(request.POST)
+    
+        if form.is_valid():
+            DriverFirstName = request.POST.get("firstName")
+            DriverMiddleName = request.POST.get("middleName")
+            DriverLastName = request.POST.get("lastName")
+            DriverEmailAddress = request.POST.get("emailAddress")
+            DriverContactNumber = request.POST.get("contactNumber")
+            DriverGender = request.POST.get("gender")
+    
+
+            form = Driver(firstName = DriverFirstName, middleName = DriverMiddleName, lastName = DriverLastName, emailAddress = DriverEmailAddress, contactNumber = DriverContactNumber, gender = DriverGender)
+            form.save()
+        
+            return redirect('head:headList_view')   
+        else:
+            print(form.errors)
+            return HttpResponse('not valid')
