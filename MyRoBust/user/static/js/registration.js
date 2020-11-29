d5213d8744ce831c290a5bbeba32ee3f7eb560aa
Kenneth Verdida <kennethverds@github.com>
2020-11-29 09:18:40 +0800

    Disabling of past dates Booking and Booking Review

--- MyRoBust/user/static/js/registration.js
+++ MyRoBust/user/static/js/registration.js
@@ -20,14 +20,14 @@
       reader.readAsDataURL(input.files[0]);
     }
   }
-  $(document).ready(function () {
-    var date_input = $('input[name="birthdate"]'); //our date input has the name "date"
-    var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
-    var options = {
-      format: 'mm/dd/yyyy',
-      container: container,
-      todayHighlight: true,
-      autoclose: true,
-    };
-    date_input.datepicker(options);
-  });
+//  $(document).ready(function () {
+//    var date_input = $('input[name="date"]'); //our date input has the name "date"
+//    var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
+//    var options = {
+//      format: 'mm/dd/yyyy',
+//      container: container,
+//      todayHighlight: true,
+//      autoclose: true,
+//    };
+//    date_input.datepicker(options);
+//  });
